ef94e9955db16eadf8ff5005e0aa5cd392cac65b
mireles <mireles@horseless.io>
2022-10-26 10:36:41 -0400

    Initial commit

---
+++ cleveland_osm.R
@@ -0,0 +1,21 @@
+library(osmdata)
+library(tidyverse)
+library(sf)
+
+#option 2 using coordinates (recommended)
+coords <- matrix(c(-81.7,-81.67,41.5,41.52),
+                 byrow = TRUE,
+                 nrow = 2,
+                 ncol = 2,
+                 dimnames = list(c('x','y'),c('min','max')))
+location <- coords %>% opq()
+
+water <- location %>%
+  add_osm_feature(key = "natural",
+                  value = c("water")) %>%
+  osmdata_sf()
+
+ggplot() +
+  geom_sf(data = water$osm_multipolygons,
+          fill = 'light blue') +
+  theme_minimal()
